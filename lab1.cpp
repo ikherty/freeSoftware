3aeb92d978f35812a81722e89966d65fecf5297a
Valentina Petrenko <valentina@altlinux.org>
2019-10-31 12:59:38 +0300

    some changes

--- lab1.cpp
+++ lab1.cpp
@@ -4,61 +4,53 @@
 using namespace std;
 
 int readfromFile(){
-HFILE              hFile;
-//Имя читаемого файла
-string vasFileName="file.txt";
-DWORD              vwCounter;
-int                *virgRDWRBuff;
-BY_HANDLE_FILE_INFORMATION bhFileInformation;
-OFSTRUCT tOfStr;
-tOfStr.cBytes = sizeof tOfStr;
+    HFILE              hFile;
+    //Имя читаемого файла
+    string vasFileName="file.txt";
+    DWORD              vwCounter;
+    int                *virgRDWRBuff;
+    BY_HANDLE_FILE_INFORMATION bhFileInformation;
+    OFSTRUCT tOfStr;
+    tOfStr.cBytes = sizeof tOfStr;
 //Открываем файл и заполняем информацией tOfStr
-hFile = OpenFile(
- vasFileName.c_str(), // имя файла, преобразуемое к типу char*
- &tOfStr,             //указатель на буфер  с информацией о файле
- OF_READWRITE);       // файл открыт для чтения и записи
-if(hFile == HFILE_ERROR)
-{
+    hFile = OpenFile(
+    vasFileName.c_str(), // имя файла, преобразуемое к типу char*
+    &tOfStr,             //указатель на буфер  с информацией о файле
+    OF_READWRITE);       // файл открыт для чтения и записи
+    if(hFile == HFILE_ERROR){
  //Здесь может быть сообщение об ощибке
- CloseHandle(HANDLE(hFile));
-return 0;
-}
+        CloseHandle(HANDLE(hFile));
+        return 0;
+    }
 //Теперь можно получить информацию о файле
-GetFileInformationByHandle
-(
- HANDLE(hFile),     // дескриптор файла
- &bhFileInformation //адрес структуры, в которой сохраняется информация
-);
+    GetFileInformationByHandle(
+    HANDLE(hFile),     // дескриптор файла
+    &bhFileInformation //адрес структуры, в которой сохраняется информация
+    );
 //Резервируем память для всего файла обычно размер файла
 //не больше  размера nFileSizeLow объявленного как DWORD
-int viSize=bhFileInformation.nFileSizeLow/sizeof(int);
-virgRDWRBuff = (int*) new int[viSize];
+    int viSize=bhFileInformation.nFileSizeLow/sizeof(int);
+    virgRDWRBuff = (int*) new int[viSize];
 //Сдвигаем указатель на нужный байт - сдесь на начало
-if(_llseek(hFile,0*sizeof(int),0) != (long)(0*sizeof(int)))
-{
- CloseHandle(HANDLE(hFile));
+    if(_llseek(hFile,0*sizeof(int),0) != (long)(0*sizeof(int))){
+        CloseHandle(HANDLE(hFile));
  //Здесь может быть сообщение об ошибке
- return 0;
-}
+        return 0;
+    }
 //Считать данные из файла
-if(!ReadFile(HANDLE(hFile),virgRDWRBuff,
-               (DWORD)viSize*sizeof(int),&vwCounter,NULL))
-{
+    if(!ReadFile(HANDLE(hFile),virgRDWRBuff,(DWORD)viSize*sizeof(int),&vwCounter,NULL)){
  //Здесь может быть сообщение об ошибке
-}
-else
-{
- if(vwCounter == (DWORD)viSize*sizeof(int))
- {
+    }
+    else{
+        if(vwCounter == (DWORD)viSize*sizeof(int)){
   //Используем информацию
-  int viRez=virgRDWRBuff[99];
- }else
- {
+            int viRez=virgRDWRBuff[99];
+        }else{
   //Здесь может быть сообщение об ошибке
- }
-}
-CloseHandle(HANDLE(hFile));
-delete[] virgRDWRBuff;
+        }
+    }
+    CloseHandle(HANDLE(hFile));
+    delete[] virgRDWRBuff;
 /*В тексте объявляются две структуры. Первая, BY_HANDLE_FILE_INFORMATION, нужна для хранения полезной информации о файле. Вторая, OFSTRUCT требуется для работы функции API OpenFile(). Далее идет инициализация поля размера этой структуры.
 
 Далее - создание буфера, в который будут скопированы данные, считанные из файла. Можно проделать расчет, основанный на размере файла, но можно поступить проще: задать размер принудительно, например 64 Кбайт. Для создания такого буфера можно использовать задание типа new char[0xfffe], выделяющей блок памяти подходящего размера. В конце обработчика необходимо освободить блок памяти операцией delete[].
@@ -76,6 +68,11 @@ delete[] virgRDWRBuff;
 При работе в переменной появятся данные из файла.*/
 }
 
+int main(){
+	readfromFile();
+	return 0;
+}
+
 int writetoFile(){
     HFILE              hFile;
 string vasFileName="file.txt";
@@ -89,8 +86,7 @@ hFile = OpenFile(
  vasFileName.c_str(), // имя файла, преобразуемое к типу char*
  &tOfStr,             //указатель на буфер  с информацией о файле
  OF_READWRITE);       // файл открыт для чтения и записи
-if(hFile == HFILE_ERROR)
-{
+if(hFile == HFILE_ERROR){
  //Здесь может быть сообщение об ощибке
  CloseHandle(HANDLE(hFile));
 return 0;
@@ -108,8 +104,7 @@ GetFileInformationByHandle
 int viSize=100;
 virgRDWRBuff = (int*) new int[viSize];
 //Сдвигаем указатель на нужный байт - сдесь на начало
-if(_llseek(hFile,0*sizeof(int),0) != (long)(0*sizeof(int)))
-{
+if(_llseek(hFile,0*sizeof(int),0) != (long)(0*sizeof(int))){
  CloseHandle(HANDLE(hFile));
  //Здесь может быть сообщение об ошибке
  return 0;
@@ -117,27 +112,17 @@ if(_llseek(hFile,0*sizeof(int),0) != (long)(0*sizeof(int)))
 //Пишем данные в буфер
 for(int i=0; i < 100; i++) virgRDWRBuff[99-i]=i;
 //Переносим содержимое буфера в файл
-if(!WriteFile(HANDLE(hFile),virgRDWRBuff,
-               (DWORD)viSize*sizeof(int),&vwCounter,NULL))
-{
+if(!WriteFile(HANDLE(hFile),virgRDWRBuff,(DWORD)viSize*sizeof(int),&vwCounter,NULL)){
  //Здесь может быть сообщение об ошибке
 }
-else
-{
- if(vwCounter == (DWORD)viSize*sizeof(int))
- {
+else{
+ if(vwCounter == (DWORD)viSize*sizeof(int)){
   //Запись прошла успешно
 
- }else
- {
+ }else{
   //Здесь может быть сообщение об ошибке
  }
 }
 CloseHandle(HANDLE(hFile));
 delete[] virgRDWRBuff;
-}
-
-int main(){
-	writetoFile();
-	return 0;
 }
