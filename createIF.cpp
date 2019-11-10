7db70ab7f4ee3a90a189ed71e21d6c369c966a0c
valentina <ikherty@gmail.com>
2019-11-10 14:24:09 +0300

    some chnfs

--- createIF.cpp
+++ createIF.cpp
@@ -2,12 +2,14 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <stdio.h>
+#include <windows.h>
 #include <stdint.h>
 using namespace std;
 
 int main(){
     ofstream outfile("InFile.txt");
-    uint8_t arr[262180];
+    byte arr[262180];
     for(int i=0;i<262180;i++){
         arr[i]=rand();
         outfile<<arr[i];
