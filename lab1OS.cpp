6ab57bb8f9031384d2af346429f74fe8402f8665
valentina <ikherty@gmail.com>
2019-11-24 18:11:48 +0300

    pochti done 1st lab

--- lab1OS.cpp
+++ lab1OS.cpp
@@ -7,14 +7,12 @@
 using namespace std;
 
 #define FileSize 256*1024           //размер файла
-#define N 1024                       //размеры блоков для чтения 
-#define NN 1024						// и записи
-#define M 20                        //сколько раз повторять для каждого размера блока
+#define block 1024
 
 int main(){
 srand(time(NULL));
 
-byte *p;                      //адрес буфера
+byte *read_b, *write_b;       //адреса буферов чтения и записи
 HANDLE DIF, DOF;              //дескрипторы файлов
 __int64 c1, c2, fr;           //переменные для тактов пр-ра, частоты
 DWORD c;
@@ -31,11 +29,42 @@ DOF=CreateFile((LPCTSTR)"OutFil.txt", GENERIC_READ|GENERIC_WRITE, 0, NULL, CREAT
 	//то же здесь можно указывать флаг для некот. случаев
 
 if(DIF==INVALID_HANDLE_VALUE || DOF==INVALID_HANDLE_VALUE){ // если файлы не открылись, выходим
-		cout << "Ошибка" << GetLastError() << endl;
-		cin.get();
-		return 0;
+    cout << "Ошибка" << GetLastError() << endl;
+    cin.get();
+    return 0;
 	}
+	
+byte *data[FileSize]; //рандомный массив для заполнения файла
+for(int i=0; i<FileSize; i++){
+    data[i]=(byte*)rand();
+    }
+WriteFile(DIF,(LPVOID)data,FileSize,&c,NULL); //заполняем файл
 
+for(int sz_buf=512; sz_buf>=64; sz_buf/=2){                  // Изменение размера буфера в 2 раза
+    read_b=(byte*)GlobalAlloc(GMEM_FIXED,1024);              // Выделение памяти под буферы чтения, записи, обмена
+    write_b=(byte*)GlobalAlloc(GMEM_FIXED,sz_buf);
+    SetFilePointer(DIF,0,NULL,FILE_BEGIN);	                  //установка файловых указателей в начала файлов
+    SetFilePointer(DOF,0,NULL,FILE_BEGIN);  
+
+         for(int k=0; k<FileSize/block;){			//цикл столько раз, сколько блоков поместится в файле
+              if(block > sz_buf) {  //если размер буфера меньше размера блока чтения
+                  ReadFile(DIF,read_b,block,&c,0); //считываем блок
+                  k++;
+                  for (int n=0; n<block/sz_buf; n++) { //делим блок чтения на несколько мелких, размер которых равен размеру буфера записи
+                      CopyMemory(write_b,read_b,sz_buf); //записываем в буфер записи
+                  }
+                  WriteFile(DOF,write_b,block,&c,0); //блок записываем в файл
+              }
+        }
+/*!!      float t=clock();
+!!        cout<<sz_buf<<" байт: ";
+!!        t=(clock()-t)/CLOCKS_PER_SEC;//для счета времени
+!!        cout<<" t = "<<t<<endl; 
+!!        cout<<"Кбайт/с = "<<500/t<<endl<<endl;*/ 
+        GlobalFree(read_b);// Освобождение памяти, занятой буферами
+        GlobalFree(write_b);
+    }
+/*
 for(int i=0; i<FileSize/4; i++)
 	WriteFile(DIF,(LPVOID)rand(),4,&c,0);
 
@@ -69,7 +98,11 @@ averageTime=averageTime/M;                                  // среднее время
 speed=FileSize/(1024*1024*averageTime);                     // скорость MB/s
 cout<<i<<"    | "<<speed<<endl;
 GlobalFree(p);                                				//освобождение памяти, занятой буфером
-}
+}*/
+
+
+    CloseHandle(DIF);
+    CloseHandle(DOF);
 //DeleteFile((LPCTSTR)"InFil.txt");
 //DeleteFile((LPCTSTR)"OutFil.txt");
 
